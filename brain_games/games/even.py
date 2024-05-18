7a42963cfa9b17f02bb203b5952c6c48eb21304b
Andrey Groza <andrey_groza@mail.ru>
2024-05-18 23:25:50 +0400

    ref even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,11 +5,11 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(num):
-    return 'yes' if num % 2 == 0 else 'no'
+    return num % 2 == 0
 
 
 def get_game_data():
     question = random.randint(0, 100)
-    answer = is_even(question)
+    answer = 'yes' if is_even(question) else 'no'
 
     return (question, answer)
